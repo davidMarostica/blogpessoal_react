f47373a8cf9fbaeee2b0b4ea7337ac395643df77
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2026-01-15 08:29:06 -0300

    criado cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
-import React from "react";
-import Home from "./pages/ home/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
+import Cadastro from "./pages/cadastro/Cadastro";
+import Home from "./pages/home/Home";
 
 function App() {
-  return <Home />;
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <div className="flex flex-col min-h-screen">
+          <div className="flex-grow flex items-center justify-center">
+            <Routes>
+              <Route path="/home" element={<Home />} />
+              <Route path="/" element={<Cadastro />} />
+            </Routes>
+          </div>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
