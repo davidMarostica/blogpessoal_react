b8220f319314fe6633a8b10a357fa4f442407a2e
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2026-01-15 14:57:49 -0300

    Organizando as pasta da pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
 import Cadastro from "./pages/cadastro/Cadastro";
 import Home from "./pages/home/Home";
-import Login from "./components/login/Login";
+import Login from "./pages/login/Login";
 
 function App() {
   return (
