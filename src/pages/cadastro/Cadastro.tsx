5762252cf29e8e20844309f101ea60e05c7427ee
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2026-01-16 12:21:02 -0300

    Integrando Back endo no Front end

--- src/pages/cadastro/Cadastro.tsx
+++ src/pages/cadastro/Cadastro.tsx
@@ -1,13 +1,80 @@
+import { useNavigate } from "react-router-dom";
+import { cadastrarUsuario } from "../../services/Service";
+import type { Usuario } from "../../models/Usuario";
+import { useEffect, useState, type ChangeEvent, type FormEvent } from "react";
+
 function Cadastro() {
+  const navigate = useNavigate();
+
+  const [loading, setIsLoading] = useState<boolean>(false);
+  const [confirmarSenha, setConfirmarSenha] = useState<string>("");
+
+  const [usuario, setUsuario] = useState<Usuario>({
+    id: 0,
+    nome: "",
+    usuario: "",
+    senha: "",
+    foto: "",
+  });
+
+  useEffect(() => {
+    if (usuario.id !== 0) {
+      return;
+    }
+  }, [usuario]);
+
+  function retornar() {
+    navigate("/login");
+  }
+
+  function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
+    setUsuario({
+      ...usuario,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  function handleConfirmarSenha(e: ChangeEvent<HTMLInputElement>) {
+    setConfirmarSenha(e.target.value);
+  }
+
+  async function cadastrar(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    if (confirmarSenha === usuario.senha && usuario.senha.length >= 8) {
+      setIsLoading(true);
+
+      try {
+        await cadastrarUsuario("/usuarios/cadastrar", usuario, setUsuario);
+        alert("Usuário cadastrado com sucesso!");
+        retornar();
+      } catch {
+        alert("Erro ao cadastrar o usuário!");
+      }
+    } else {
+      alert(
+        "Dados do usuário inconsistentes! Verifique as informações do cadastro."
+      );
+      setUsuario({ ...usuario, senha: "" });
+      setConfirmarSenha("");
+    }
+
+    setIsLoading(false);
+  }
+
   return (
     <>
       <div className="grid grid-cols-1 lg:grid-cols-2 w-full h-screen">
         <div
           className="bg-[url('https://i.imgur.com/ZZFAmzo.jpg')] lg:block hidden bg-no-repeat 
-                    w-full h-screen bg-cover bg-center"
+                      w-full h-screen bg-cover bg-center"
         ></div>
-        <form className="flex justify-center items-center flex-col w-full px-8 py-12 gap-3">
+        <form
+          onSubmit={cadastrar}
+          className="flex justify-center items-center flex-col w-full px-8 py-12 gap-3"
+        >
           <h2 className="text-slate-900 text-5xl">Cadastrar</h2>
+
           <div className="flex flex-col w-full">
             <label htmlFor="nome">Nome</label>
             <input
@@ -16,18 +83,24 @@ function Cadastro() {
               name="nome"
               placeholder="Nome"
               className="border-2 border-slate-700 rounded p-2"
+              value={usuario.nome}
+              onChange={atualizarEstado}
             />
           </div>
+
           <div className="flex flex-col w-full">
-            <label htmlFor="usuario">Usuario</label>
+            <label htmlFor="usuario">Usuário</label>
             <input
               type="text"
               id="usuario"
               name="usuario"
-              placeholder="Usuario"
+              placeholder="Usuário"
               className="border-2 border-slate-700 rounded p-2"
+              value={usuario.usuario}
+              onChange={atualizarEstado}
             />
           </div>
+
           <div className="flex flex-col w-full">
             <label htmlFor="foto">Foto</label>
             <input
@@ -36,8 +109,11 @@ function Cadastro() {
               name="foto"
               placeholder="Foto"
               className="border-2 border-slate-700 rounded p-2"
+              value={usuario.foto}
+              onChange={atualizarEstado}
             />
           </div>
+
           <div className="flex flex-col w-full">
             <label htmlFor="senha">Senha</label>
             <input
@@ -46,8 +122,11 @@ function Cadastro() {
               name="senha"
               placeholder="Senha"
               className="border-2 border-slate-700 rounded p-2"
+              value={usuario.senha}
+              onChange={atualizarEstado}
             />
           </div>
+
           <div className="flex flex-col w-full">
             <label htmlFor="confirmarSenha">Confirmar Senha</label>
             <input
@@ -56,8 +135,11 @@ function Cadastro() {
               name="confirmarSenha"
               placeholder="Confirmar Senha"
               className="border-2 border-slate-700 rounded p-2"
+              value={confirmarSenha}
+              onChange={handleConfirmarSenha}
             />
           </div>
+
           <div className="flex justify-around w-full gap-8">
             <button
               type="reset"
@@ -68,10 +150,10 @@ function Cadastro() {
             <button
               type="submit"
               className="rounded text-white bg-indigo-400 
-                           hover:bg-indigo-900 w-1/2 py-2
-                           flex justify-center"
+                         hover:bg-indigo-900 w-1/2 py-2
+                         flex justify-center"
             >
-              Cadastrar
+              {loading ? "Cadastrando..." : "Cadastrar"}
             </button>
           </div>
         </form>
